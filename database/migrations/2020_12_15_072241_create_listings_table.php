ae0ad4549217441806aa67a8f234c035b904612d
Muaz Wazir <muazwazir@gmail.com>
2020-12-15 15:45:06 +0800

    image & listing

---
+++ database/migrations/2020_12_15_072241_create_listings_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateListingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('listings', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->enum('delivery_type', ['cod', 'postage']);
+            $table->string('contact');
+            $table->timestamps();
+            $table->enum('status', ['available', 'sold']);
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('image_id');
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('listings');
+    }
+}
---
+++ database/migrations/2020_12_15_072656_create_images_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->id();
+            $table->binary('binary');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::table('listings', function (Blueprint $table) {
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('image_id')->references('id')->on('images');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('images');
+    }
+}
