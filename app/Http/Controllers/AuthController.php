587f014509bc15e778df4bfc96befa745b3a4bca
Muaz Wazir <muazwazir@gmail.com>
2020-12-17 02:11:29 +0800

    routes and user controller

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class AuthController extends Controller
+{
+    //
+    public function register(Request $req)
+    {
+        //dd($req);
+        $validate = $req->validate([
+            'name' => ['required', 'max:55'],
+            'email' => ['email', 'required'],
+            'password' => ['required'],
+            'role' => ['required']
+        ]);
+        $validate['password'] = bcrypt($req->password);
+
+        $user = User::create($validate);
+        $token = $user->createToken('authToken')->accessToken;
+
+        return response(['user' => $user, 'access_token' => $token]);
+    }
+
+    public function login(Request $req)
+    {
+        $login = $req->validate([
+            'email' => 'email|required',
+            'password' => 'required'
+        ]);
+
+        if (!auth()->attempt($login)) return response(['message' => 'Invalid credentials']);
+
+        $accessToken = auth()->user()->createToken('authToken')->accessToken;
+        return response(['user' => auth()->user(), 'access_token' => $accessToken]);
+    }
+}
---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::all();
+        return response(['users' => UserResource::collection($users), 'message' => 'Retrieved successfully'], 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function show(User $user)
+    {
+        return response(['user' => new UserResource($user), 'message' => 'Retrieved successfully'], 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, User $user)
+    {
+        $user->update($request->all());
+
+        return response(['user' => new UserResource($user), 'message' => 'Retrieved successfully'], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(User $user)
+    {
+        $user->delete();
+
+        return response(['message' => 'Deleted']);
+    }
+}
